ed36157195426c4f4a1500a438eeefa9ddbbb035
OriBiton <ori1230302@gmail.com>
2025-05-11 13:24:47 +0300

    Update settings.py

--- myapp/settings.py
+++ myapp/settings.py
@@ -73,19 +73,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# ✅ נוספה הגדרת לוגינג שתפתור את ה־crash
-LOGGING_CONFIG = 'logging.config.dictConfig'
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'root': {
-        'handlers': ['console'],
-        'level': 'INFO',
-    },
-}
+LOGGING_CONFIG = None
+
 
