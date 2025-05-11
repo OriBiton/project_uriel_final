aacff5c9d4d3861989cd6d2a5e865b61eb314c0d
OriBiton <ori1230302@gmail.com>
2025-05-11 13:29:23 +0300

    Update wsgi.py

--- myapp/wsgi.py
+++ myapp/wsgi.py
@@ -1,15 +1,8 @@
-"""
-WSGI config for myapp project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-
+import django
 from django.core.wsgi import get_wsgi_application
+
+print("💡 WSGI start loading...")  # שורת בדיקה
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myapp.settings')
 
