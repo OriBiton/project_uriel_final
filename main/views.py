c27c0964f5dde9ba77cc2444c2317ed64a8a3c56
OriBiton <ori1230302@gmail.com>
2025-05-12 06:03:12 +0000

    ✨ הוספתי לוגים לבדיקה

--- main/views.py
+++ main/views.py
@@ -165,10 +165,15 @@ from django.contrib.auth import get_user_model
 # views.py
 from django.contrib import messages
 from django.shortcuts import redirect
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+
 @login_required
 def logout_view(request):
     logout(request)
     return redirect('login')
+
 
 @login_required
 @csrf_exempt
