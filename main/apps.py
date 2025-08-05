df1c95c9d925b92b2e0b6eaaacb53ee972fe9c4f
OriBiton <ori1230302@gmail.com>
2025-08-05 08:40:17 +0300

    Update apps.py

--- main/apps.py
+++ main/apps.py
@@ -1,18 +1,23 @@
 from django.apps import AppConfig
 from django.contrib.auth import get_user_model
 import logging
+from django.db.utils import OperationalError, ProgrammingError
 
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
 
     def ready(self):
-        User = get_user_model()
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser(
-                username='0544403199',
-                password='Ori192837465',
-                email='admin@example.com'
-            )
-            logging.warning("✔ נוצר משתמש סופר-יוזר בשם admin עם סיסמה admin123")
+        try:
+            User = get_user_model()
+            if not User.objects.filter(username='admin').exists():
+                User.objects.create_superuser(
+                    username='admin',
+                    password='admin123',
+                    email='admin@example.com'
+                )
+                logging.warning("✔ נוצר משתמש סופר-יוזר בשם admin עם סיסמה admin123")
+        except (OperationalError, ProgrammingError):
+            # עדיין אין טבלאות – מתעלמים מהשגיאה
+            pass
 
